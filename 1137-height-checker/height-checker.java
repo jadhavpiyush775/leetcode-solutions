bc290c7db1bda974d69d4271695bbafc831be650
jadhavpiyush775 <jadhavpiyush775@gmail.com>
2026-09-10 23:29:59 +0530

    Time: 5 ms (34.16%) | Memory: 43.5 MB (27.44%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,33 +1,17 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int n=heights.length;
-        int []expected=new int[n];
-        int count=0;
-        for(int i=0;i<n;i++){
-            expected[i]=heights[i];
-            }
-            sort(expected);
-            for(int j=0;j<n;j++){
-                if(expected[j]!=heights[j]){
-                    count++;
-                }
-            }
-            return count;
+    int n=heights.length;
+    int count=0;
+    int []expected=new int [n];
+    for(int i=0;i<n;i++){
+        expected[i]=heights[i];
     }
-
-    static void sort(int []expected){
-       for(int i=0;i<expected.length-1;i++){
-        int min=i;
-        for(int j=i+1;j<expected.length;j++){
-            if(expected[j]<expected[min]){
-                min=j;
-            }
-        }swap(expected,i,min);
+    Arrays.sort(expected);
+    for(int j=0;j<n;j++){
+        if(expected[j]!=heights[j]){
+            count++;
+        }
     }
-    }
-    static void swap(int []expected,int first,int second){
-        int temp=expected[first];
-        expected[first]=expected[second];
-        expected[second]=temp;
+    return count;
     }
 }

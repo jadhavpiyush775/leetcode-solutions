5a4ae56a0c765dd362807a381f47e8d3f7b8b108
jadhavpiyush775 <jadhavpiyush775@gmail.com>
2026-09-21 20:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (10.65%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+          
+    
+     
+
+
+        if(x!=0){
+            return Math.pow(x, n);
+        }else{
+            return 0.00;
+        }
+     }
+    
+}
+
+     

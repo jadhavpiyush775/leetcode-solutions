0401d5812864ff15702c9bb2049bca2792c557dd
jadhavpiyush775 <jadhavpiyush775@gmail.com>
2026-09-22 00:06:15 +0530

    Time: 2 ms (99.30%) | Memory: 44.3 MB (73.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,20 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String s0=s.toLowerCase();
-        String s1=s0.replaceAll("[^a-z0-9]","");
-        char []arr=s1.toCharArray();
         int i=0;
-        int j=arr.length-1;
+        int j=s.length()-1;
         while(i<j){
-            if(arr[i]!=arr[j]){
-                return false;
-            }else{
-                i++;
-                j--;
-                
+            char left=s.charAt(i);
+            char right=s.charAt(j);
+            if(!Character.isLetterOrDigit(left)){
+              i= i+1;
+              continue;
             }
+            if(!Character.isLetterOrDigit(right)){
+                j=j-1;
+            continue;
+            }
+            if(Character.toLowerCase(left)!=Character.toLowerCase(right)){
+                return false;
+            }
+            i=i+1;
+            j=j-1;
 
-        }
+        }   
         return true;
-    }
+        
+         }
 }
